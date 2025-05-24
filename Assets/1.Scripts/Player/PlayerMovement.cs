a250db1cd4cea3a687077052e1bbce895b94691c
rese1740 <rese1748@naver.com>
2025-05-25 01:19:07 +0900

    [Feat] 플레이어 데이터

--- Assets/1.Scripts/Player/PlayerMovement.cs
+++ Assets/1.Scripts/Player/PlayerMovement.cs
@@ -1,26 +1,38 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-
     private Rigidbody2D rb;
     private Vector2 moveInput;
-
     [Header("애니메이션")]
     Animator animator;
     bool facingRight = true;
+    public Light2D spotLight2D;
 
+    [Header("PlayerStat")]
+    public PlayerSO playerData;
+    public float currentHp;
+    public float maxHp;
+    public float moveSpeed = 5f;
+    public float damage = 5f;
 
-    void Awake()
+    [Header("PlayerStat")]
+    public float minRadius = 1f;   // HP 0일 때 최소 반경
+    public float maxRadius = 5f;   // HP 최대일 때 최대 반경
+
+    void Start ()
     {
+        PlayerStatReload();
+        currentHp = maxHp;
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
     }
 
     void Update()
     {
+        #region 이동
         // 입력 받기
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
@@ -38,12 +50,27 @@ public class PlayerMovement : MonoBehaviour
         {
             Flip();
         }
+        #endregion
 
+        #region 빛
+        float hpRatio = Mathf.Clamp01((float)currentHp / maxHp);
 
+        spotLight2D.pointLightOuterRadius = Mathf.Lerp(minRadius, maxRadius, hpRatio);
+
+        spotLight2D.pointLightInnerRadius = spotLight2D.pointLightOuterRadius * 0.7f;
+        #endregion
+
+        #region 공격
         if (Input.GetKeyDown(KeyCode.Z))
         {
             animator.SetTrigger("Attack");
         }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            animator.SetTrigger("Thrust");
+        }
+        #endregion
     }
 
     void Flip()
@@ -53,6 +80,19 @@ public class PlayerMovement : MonoBehaviour
         Vector3 scale = transform.localScale;
         scale.x *= -1;
         transform.localScale = scale;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHp -= damage;
+        currentHp = Mathf.Max(currentHp, 0);
+    }
+
+    void PlayerStatReload()
+    {
+        maxHp = playerData.maxHp;
+        moveSpeed = playerData.moveSpeed;
+        damage = playerData.damage;
     }
 
     void FixedUpdate()
---
+++ Assets/1.Scripts/Player/PlayerSO.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PlayerSO : ScriptableObject
+{
+    public float currerntHp = 0f;
+    public float maxHp = 0f;
+    public float moveSpeed = 5f;
+    public float damage = 5f;
+
+}
