9277e2cd093b1f44649fa112aa135c06a64e1d49
rese1740 <rese1748@naver.com>
2025-06-17 16:01:48 +0900

    [Feat] 집가서 수정 ^^

--- Assets/1.Scripts/Item/ShopItem.cs
+++ Assets/1.Scripts/Item/ShopItem.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class ShopItem : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] items;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+   void RandomItem()
     {
-        
+       
     }
 }
